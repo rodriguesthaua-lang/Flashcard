d73eeba0b25e87955222119278e038793c76cbc1
rodriguesthaua-lang <rodrigues.thaua@escola.pr.gov.br>
2025-11-14 12:17:55 -0300

    Add programming cards for JavaScript, CSS, and HTML

---
+++ app.js
@@ -0,0 +1,17 @@
+criaCartao(
+    'Programação',
+    'O que é JavaScript?',
+    'O JavaScript é uma linguagem de programação usada para criar interatividade em páginas web, permitindo que elas respondam a ações do usuário, como cliques ou movimentos do mouse.'
+)
+
+criaCartao(
+    'Programação',
+    'O que é css?',
+    'O css é uma linguagem de estilização, permitindo aplicar estilos diretamente nos elementos HTML, alterando sua aparência visual.'
+)
+
+criaCartao(
+    'Programação',
+    'O que é html?',
+    'HTML, ou Linguagem de Marcação de Hipertexto, é a linguagem padrão usada para criar e estruturar páginas web.'
+)
