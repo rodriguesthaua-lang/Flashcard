b0220157f55d113410bd3bb5a0d0511c2f171104
rodriguesthaua-lang <rodrigues.thaua@escola.pr.gov.br>
2025-11-14 12:17:09 -0300

    Create perguntas.js

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Programação',
+    'O que é JavaScript?',
+    'O JavaScript é uma linguagem de programação usada para criar interatividade em páginas web, permitindo que elas respondam a ações do usuário, como cliques ou movimentos do mouse.'
+)
+
+criaCartao(
+    'Programação',
+    'O que é css?',
+    'O css é uma linguagem de estilização, permitindo aplicar estilos diretamente nos elementos HTML, alterando sua aparência visual.'
+)
+
+criaCartao(
+    'Programação',
+    'O que é html?',
+    'HTML, ou Linguagem de Marcação de Hipertexto, é a linguagem padrão usada para criar e estruturar páginas web.'
+)
+
+criaCartao(
+    'História',
+    'Em que ano cemçou a 1 guerra mundial?',
+    'começou 28 de julho de 1914'
+)
+
+criaCartao(
+    'Geofrafia',
+    'Qual o nome do rio que atravessa londres?',
+    'O rio que atravessa Londres se chama Rio tâmisa'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é o pais com mais ilhas no mundo?',
+    'O pais com mais olhas no mundo e a suécia'
+)
